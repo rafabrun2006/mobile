b63eea4c93cbc85140c525536a23adeb93c6e3a4
Rafael Bruno <rafabrun2006@gmail.com>
2014-08-14 02:40:44 -0300

    organizacao e criacao de funcionalidades reais

--- MainAngular.js
+++ MainAngular.js
@@ -8,6 +8,20 @@ App.factory('Fbase', function ($firebase, fbURL) {
     return $firebase(new Firebase(fbURL)).$asArray();
 });
 
+App.factory('FbaseSimpleLogin', function (fbURL) {
+    var Fire = new Firebase(fbURL);
+
+    return new FirebaseSimpleLogin(Fire, function (error, user) {
+    });
+});
+
+App.factory('Pessoa', function () {
+    return Pessoa = {
+        email: '',
+        senha: ''
+    };
+});
+
 App.controller('Main', function ($log, $scope, $location) {
 
     if (localStorage.getItem('session')) {
@@ -18,26 +32,25 @@ App.controller('Main', function ($log, $scope, $location) {
     if ($scope.session) {
         $log.info('data');
     } else {
+        $log.info('usuario nao logado');
         window.location.assign('#login');
+    }
+
+    $scope.logout = function () {
+        navigator.app.exitApp();
     }
 
 });
 
-App.controller('Cadastro', function ($log, $scope, $location, Fbase, fbURL) {
+App.controller('Cadastro', function ($log, $scope, $location, Fbase, FbaseSimpleLogin, Pessoa) {
 
-    $scope.Pessoa = {
-        email: 'rafaelbruno@gmail.com',
-        senha: 'ranelore'
-    };
+    $scope.Pessoa = Pessoa;
 
     localStorage.setItem('data', $scope.Pessoa);
 
     $scope.novoRegistro = function () {
 
-        var Fire = new Firebase(fbURL);
-        var SimpleLogin = new FirebaseSimpleLogin(Fire, function (error, user){});
-
-        SimpleLogin.createUser($scope.Pessoa.email, $scope.Pessoa.senha, function (error, user) {
+        var result = FbaseSimpleLogin.createUser($scope.Pessoa.email, $scope.Pessoa.senha, function (error, user) {
             if (error === null) {
 
                 localStorage.setItem('session', JSON.stringify(user));
@@ -46,32 +59,60 @@ App.controller('Cadastro', function ($log, $scope, $location, Fbase, fbURL) {
                 window.location.assign('#main');
 
             } else {
-                $log.info(error.error.code);
-                switch (error.error.code) {
+                $log.debug('Conta ja registrada na base de dados');
+                switch (result._detail.code) {
                     case 'EMAIL_TAKEN':
-                        alert('Este registro já se encontra em nossa base de dados');
+                        alert('Esta conta já esta registrada.');
                         break;
                 }
             }
         });
     };
 
-    $scope.esqueciMinhaSenha = function () {
-        if ($scope.Pessoa.email) {
-            alert('Enviamos um email para a sua caixa com informações de como proceder para recuperar sua senha.')
-        }
-    };
-
-    $scope.back = function(){
-        $log.info('#' + url);
-        window.location.assign('#' + url);
+    $scope.back = function () {
+        $log.info(url);
+        window.location.assign(url);
     };
 
 });
 
-App.controller('Login', function($log, $scope, fbURL){
-    $scope.cadastro = function(){
-        url = window.location.hash;
+App.controller('Login', function ($log, $scope, FbaseSimpleLogin, Pessoa) {
+
+    $scope.Pessoa = Pessoa;
+
+    $scope.login = function () {
+
+        if ($scope.Pessoa.email && $scope.Pessoa.senha) {
+            FbaseSimpleLogin.login('password', {
+                email: Pessoa.email,
+                password: Pessoa.senha,
+                rememberMe: true
+            }).then(function (response) {
+
+                localStorage.setItem('session', JSON.stringify(response));
+                window.location.assign('#main');
+
+            }).catch(function (response) {
+                alert('Usuário ou Senha incorreto(s)');
+            });
+        } else {
+            alert('Preencha os campos Email e Senha');
+        }
+
+    };
+
+    $scope.cadastro = function () {
+        url = '#login';
         window.location.assign('#cadastro');
-    }
+    };
+
+    $scope.esqueciMinhaSenha = function () {
+        $log.info('Funcao esqueci minha senha');
+        if ($scope.Pessoa.email) {
+            alert('Enviamos um email para a sua caixa com informações de como proceder para recuperar sua senha.')
+        } else {
+            alert('Preencha o email cadastrado em sua conta');
+        }
+    };
+
 });
